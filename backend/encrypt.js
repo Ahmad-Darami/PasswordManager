597be664fd4816be73084ddc9cb6c57e14a0d506
Ahmad <darami2022@gmail.com>
2025-04-29 11:42:18 -0400

    encryption algo

---
+++ backend/encrypt.js
@@ -0,0 +1,32 @@
+import {ethers} from 'ethers';
+
+//signer will become available from ethers
+
+export const getEncryptionKey = async (signer) => {
+    const SignedMessage = await signer.signMessage('Blockpass Key Generation')
+    // after this, a signed message should appear.
+
+    return ethers.utils.keccak256(ethers.utils.toUtf8Bytes(SignedMessage));
+
+}
+
+// Encrypt text using derived key
+
+export const encryptText = async (text, signer) => {
+  const key = await getEncryptionKey(signer);
+  const textBytes = ethers.utils.toUtf8Bytes(text);
+  const encryptedBytes = textBytes.map((b, i) =>
+    b ^ parseInt(key.slice(2 + (i % 64), 4 + (i % 64)), 16)
+  );
+  return ethers.utils.hexlify(encryptedBytes);
+};
+
+// Decrypt text using derived key
+export const decryptText = async (hex, signer) => {
+  const key = await getEncryptionKey(signer);
+  const encryptedBytes = ethers.utils.arrayify(hex);
+  const decryptedBytes = encryptedBytes.map((b, i) =>
+    b ^ parseInt(key.slice(2 + (i % 64), 4 + (i % 64)), 16)
+  );
+  return ethers.utils.toUtf8String(decryptedBytes);
+};
