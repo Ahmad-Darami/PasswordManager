d1079f0be4698be617375c81d2a78203f74f323a
Ahmad <darami2022@gmail.com>
2025-05-02 15:46:58 -0400

    Secret note now works and sends notes to blockchain

--- pages/NewSecretNote.js
+++ pages/NewSecretNote.js
@@ -4,7 +4,7 @@ import Footer from '@/components/LandingPage/Footer';
 import React from 'react';
 import {useState,useEffect} from "react";
 import {ethers, Signer} from 'ethers';
-import contractABI from '@/contracts/SecretStore.json'
+import {CONTRACT_ABI, CONTRACT_ADDRESS} from '@/contracts/contract'
 import { encryptText, decryptText, getEncryptionKey } from "@/backend/encrypt";
 import { useStateContext } from "@/context/StateContext";
 
@@ -12,18 +12,40 @@ import { useStateContext } from "@/context/StateContext";
 const NewSecretNote = () => {
   const [Textbody, setTextbody] = useState('');
   const [EncryptedText, SetEncryptedText] = useState('');
+  const [response,setResponse] = useState('');
+  const [NoteHint, setNoteHint] = useState('');
   const [Timestamp, setTimestamp] = useState('');
-
   const {signer} = useStateContext();
   const WalletAddress = "empty address"
 
-  const encrypt = async () =>{
-    const key = await getEncryptionKey(signer);
-    const encrypted = await encryptText(Textbody, signer)
-    console.log('encrypted', encrypted)
-    
+
+  const encryptAndSend = async() => {
+    if (!signer) {
+      setResponse("Wallet not Connected")
+      return;
+    }
+    if (!Textbody) {
+      setResponse("Please enter something to encrypt")
+      return;
+    }
+    if (!NoteHint) {
+      setResponse("Please provide a hint")
+      return;
+    }
+    try {
+      const encrypted = await encryptText(Textbody, signer)
+      console.log('encrypted', encrypted)
+      SetEncryptedText(encrypted)
+      const contract = new ethers.Contract(CONTRACT_ADDRESS,CONTRACT_ABI,signer)
+      const tx = await contract.StoreSecretNote(encrypted,NoteHint)
+      const receipt = await tx.wait();
+      console.log(receipt)
+
+    } catch (err) {
+      console.error("❌ Error in encrypting or uploading:", err);
+      SetEncryptedText("❌ Upload failed.");
+    }
   };
-  
 
   return (
       <>
@@ -38,9 +60,15 @@ const NewSecretNote = () => {
       value = {Textbody}
       onChange={(e) => setTextbody(e.target.value)}
       ></InputText>
+
+      <InputHint 
+      placeholder='add a hint (WARNING: this is decrypted!)'
+      value = {NoteHint}
+      onChange={(e) => setNoteHint(e.target.value)}
+      ></InputHint>
       
-      <SubmitButton onClick={(e) => encrypt()}>submit</SubmitButton>
-      {EncryptedText && (
+      <SubmitButton onClick={(e) => encryptAndSend()}>Encrypt and Send</SubmitButton>
+      {setResponse && (
         <p style={{ marginTop: '10px', wordBreak: 'break-all' }}>
           🔒 Encrypted Text: <br /> {EncryptedText}
         </p>
@@ -71,6 +99,15 @@ const InputText = styled.textarea`
   font-size:50px;
   margin:25px;
 
+`
+
+const InputHint = styled.textarea`
+  height: 8vh;
+  width: 30vh;
+  resize:none;
+  font-size:20px;
+  margin:25px;
+  text-align:center;
 `
 
 const ElementSection = styled.div`
