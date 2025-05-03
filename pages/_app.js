73fdd60b3aa70d5e18f3c58f1b45c9e7bfd6d0b3
Ahmad <darami2022@gmail.com>
2025-05-02 23:34:28 -0400

    final commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import { StateContext } from "@/context/StateContext"
 import { createGlobalStyle } from 'styled-components'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 export const GlobalStyle = createGlobalStyle`
   * 
@@ -13,13 +16,31 @@ export const GlobalStyle = createGlobalStyle`
 `
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  const [routeChanging, setRouteChanging] = useState(false)
+
+  useEffect(() => {
+    const handleStart = () => setRouteChanging(true)
+    const handleComplete = () => setRouteChanging(false)
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  }, [router])
+
   return (
     <>
     
         <Head>
           <title>BlockPass</title>
-          <meta name='description' content='Put a description here about your app'/>
-          <meta name='robots' content='index, follow'/>
+          <meta name='description' content='The best decentralized password manager out there.'/>
+          <meta name='bepis' content='index'/>
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon_package/apple-touch-icon.png"/>
           {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon_package/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon_package/favicon-16x16.png"/> */}
@@ -31,6 +52,17 @@ export default function App({ Component, pageProps }) {
         <GlobalStyle />
 
       <StateContext>
+      <AnimatePresence mode="wait">
+          <motion.div
+            key={router.route}
+            initial={{ opacity: 0, filter: 'blur(10px)' }}
+            animate={{ opacity: 1, filter: 'blur(0px)' }}
+            exit={{ opacity: 0, filter: 'blur(8px)' }}
+            transition={{ duration: 0.18, ease: 'easeInOut' }}
+          >
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
         <Component {...pageProps} />
       </StateContext>
     </>
